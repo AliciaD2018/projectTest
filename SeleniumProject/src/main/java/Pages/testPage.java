7cc418023ac23b83c0b79824c10441a503d68865
AliciaD2018 <aliciadiazrivas@gmail.com>
2024-01-08 09:12:21 -0600

    Fist Commit

---
+++ SeleniumProject/src/main/java/Pages/testPage.java
@@ -0,0 +1,18 @@
+package Pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class testPage {
+
+    static WebDriver driver;
+
+    public testPage(WebDriver remoteDriver){
+        driver = remoteDriver;
+    }
+    public static void singIn(String username, String password){
+
+
+    }
+}
